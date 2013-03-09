10e9d3a6d06f0da1f0a39ba45ade08740a8f07f4
José Cabo <folcwine@gmail.com>
2013-03-09 19:25:25 +0100

    step-1

--- introduction.js_files/introduction.js
+++ introduction.js_files/introduction.js
@@ -37,6 +37,161 @@ describe("A suite is just a function", function() {
 });
 
 /**
+ ## Expectations
+
+ Expectations are built with the function `expect` which takes a value, called the actual. It is chained with a Matcher function, which takes the expected value.
+ */
+describe("The 'toBe' matcher compares with ===", function() {
+  /**
+   ### Matchers
+
+   Each matcher implements a boolean comparison between the actual value and the expected value. It is responsible for reporting to Jasmine if the expectation is true or false. Jasmine will then pass or fail the spec.
+   */
+
+  it("and has a positive case ", function() {
+    expect(true).toBe(true);
+  });
+
+  /**
+   Any matcher can evaluate to a negative assertion by chaining the call to `expect` with a `not` before calling the matcher.
+
+   */
+
+  it("and can have a negative case", function() {
+    expect(false).not.toBe(true);
+  });
+});
+
+/**
+ ### Included Matchers
+
+ Jasmine has a rich set of matchers included. Each is used here - all expectations and specs pass.
+
+ There is also the ability to write [custom matchers](https://github.com/pivotal/jasmine/wiki/Matchers) for when a project's domain calls for specific assertions that are not included below.
+ */
+
+describe("Included matchers:", function() {
+
+  it("The 'toBe' matcher compares with ===", function() {
+    var a = 12;
+    var b = a;
+
+    expect(a).toBe(b);
+    expect(a).not.toBe(null);
+  });
+
+  describe("The 'toEqual' matcher", function() {
+
+    it("works for simple literals and variables", function() {
+      var a = 12;
+      expect(a).toEqual(12);
+    });
+
+    it("should work for objects", function() {
+      var foo = {
+        a: 12,
+        b: 34
+      };
+      var bar = {
+        a: 12,
+        b: 34
+      };
+      expect(foo).toEqual(bar);
+    });
+  });
+
+  it("The 'toMatch' matcher is for regular expressions", function() {
+    var message = 'foo bar baz';
+
+    expect(message).toMatch(/bar/);
+    expect(message).toMatch('bar');
+    expect(message).not.toMatch(/quux/);
+  });
+
+  it("The 'toBeDefined' matcher compares against `undefined`", function() {
+    var a = {
+      foo: 'foo'
+    };
+
+    expect(a.foo).toBeDefined();
+    expect(a.bar).not.toBeDefined();
+  });
+
+  it("The `toBeUndefined` matcher compares against `undefined`", function() {
+    var a = {
+      foo: 'foo'
+    };
+
+    expect(a.foo).not.toBeUndefined();
+    expect(a.bar).toBeUndefined();
+  });
+
+  it("The 'toBeNull' matcher compares against null", function() {
+    var a = null;
+    var foo = 'foo';
+
+    expect(null).toBeNull();
+    expect(a).toBeNull();
+    expect(foo).not.toBeNull();
+  });
+
+  it("The 'toBeTruthy' matcher is for boolean casting testing", function() {
+    var a, foo = 'foo';
+
+    expect(foo).toBeTruthy();
+    expect(a).not.toBeTruthy();
+  });
+
+  it("The 'toBeFalsy' matcher is for boolean casting testing", function() {
+    var a, foo = 'foo';
+
+    expect(a).toBeFalsy();
+    expect(foo).not.toBeFalsy();
+  });
+
+  it("The 'toContain' matcher is for finding an item in an Array", function() {
+    var a = ['foo', 'bar', 'baz'];
+
+    expect(a).toContain('bar');
+    expect(a).not.toContain('quux');
+  });
+
+  it("The 'toBeLessThan' matcher is for mathematical comparisons", function() {
+    var pi = 3.1415926, e = 2.78;
+
+    expect(e).toBeLessThan(pi);
+    expect(pi).not.toBeLessThan(e);
+  });
+
+  it("The 'toBeGreaterThan' is for mathematical comparisons", function() {
+    var pi = 3.1415926, e = 2.78;
+
+    expect(pi).toBeGreaterThan(e);
+    expect(e).not.toBeGreaterThan(pi);
+  });
+
+  it("The 'toBeCloseTo' matcher is for precision math comparison", function() {
+    var pi = 3.1415926, e = 2.78;
+
+    expect(pi).not.toBeCloseTo(e, 2);
+    expect(pi).toBeCloseTo(e, 0);
+  });
+
+  it("The 'toThrow' matcher is for testing if a function throws an exception", function() {
+    var foo = function() {
+      return 1 + 2;
+    };
+    var bar = function() {
+      return a + 1;
+    };
+
+    expect(foo).not.toThrow();
+    expect(bar).toThrow();
+  });
+});
+
+
+/**
  ## The Runner and Reporter
 
  Jasmine is built in JavaScript and must be included into a JS environment, such as a web page, in order to run. Like this web page.
