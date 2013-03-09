fe526650af361f5c76cbd1dbb44e26ff2b159a18
José Cabo <folcwine@gmail.com>
2013-03-09 19:41:16 +0100

    step-2

--- introduction.js_files/introduction.js
+++ introduction.js_files/introduction.js
@@ -190,6 +190,469 @@ describe("Included matchers:", function() {
   });
 });
 
+/**
+ ## Grouping Related Specs with `describe`
+
+ The `describe` function is for grouping related specs. The string parameter is for naming the collection of specs, and will be contatenated with specs to make a spec's full name. This aids in finding specs in a large suite. If you name them well, your specs read as full sentences in traditional [BDD][bdd] style.
+
+ [bdd]: http://en.wikipedia.org/wiki/Behavior-driven_development
+ */
+describe("A spec", function() {
+  it("is just a function, so it can contain any code", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+});
+
+/**
+ ### Setup and Teardown
+
+ To help a test suite DRY up any duplicated setup and teardown code, Jasmine provides the global `beforeEach` and `afterEach` functions. As the name implies the `beforeEach` function is called once before each spec in the `describe` is run and the `afterEach` function is called once after each spec.
+
+ Here is the same set of specs written a little differently. The variable under test is defined at the top-level scope -- the `describe` block --  and initialization code is moved into a `beforeEach` function. The `afterEach` function resets the variable before continuing.
+
+ */
+
+describe("A spec (with setup and tear-down)", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  afterEach(function() {
+    foo = 0;
+  });
+
+  it("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+});
+
+/**
+ ### Nesting `describe` Blocks
+
+ Calls to `describe` can be nested, with specs defined at any level. This allows a suite to be composed as a tree of functions. Before a spec is executed, Jasmine walks down the tree executing each `beforeEach` function in order. After the spec is executed, Jasmine walks through the `afterEach` functions similarly.
+
+ */
+describe("A spec", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  afterEach(function() {
+    foo = 0;
+  });
+
+  it("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+
+  describe("nested inside a second describe", function() {
+    var bar;
+
+    beforeEach(function() {
+      bar = 1;
+    });
+
+    it("can reference both scopes as needed ", function() {
+      expect(foo).toEqual(bar);
+    });
+  });
+});
+
+/**
+ ## Disabling Specs and Suites
+
+ Suites and specs can be disabled with the `xdescribe` and `xit` functions, respectively. These suites and specs are skipped when run and thus their results will not appear in the results.
+
+ */
+xdescribe("A spec", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  xit("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+});
+
+/**
+ ## Spies
+
+ Jasmine's test doubles are called spies. A spy can stub any function and tracks calls to it and all arguments. There are special matchers for interacting with spies.
+
+ The `toHaveBeenCalled` matcher will return true if the spy was called. The `toHaveBeenCalledWith` matcher will return true if the argument list matches any of the recorded calls to the spy.
+ */
+
+describe("A spy", function() {
+  var foo, bar = null;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      }
+    };
+
+    spyOn(foo, 'setBar');
+
+    foo.setBar(123);
+    foo.setBar(456, 'another param');
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(foo.setBar).toHaveBeenCalled();
+  });
+
+  it("tracks its number of calls", function() {
+    expect(foo.setBar.calls.length).toEqual(2);
+  });
+
+  it("tracks all the arguments of its calls", function() {
+    expect(foo.setBar).toHaveBeenCalledWith(123);
+    expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
+  });
+
+  it("allows access to the most recent call", function() {
+    expect(foo.setBar.mostRecentCall.args[0]).toEqual(456);
+  });
+
+  it("allows access to other calls", function() {
+    expect(foo.setBar.calls[0].args[0]).toEqual(123);
+  });
+
+  it("stops all execution on a function", function() {
+    expect(bar).toBeNull();
+  });
+});
+
+/**
+ ### Spies: `andCallThrough`
+
+ By chaining the spy with `andCallThrough`, the spy will still track all calls to it but in addition it will delegate to the actual implementation.
+ */
+describe("A spy, when configured to call through", function() {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      },
+      getBar: function() {
+        return bar;
+      }
+    };
+
+    spyOn(foo, 'getBar').andCallThrough();
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it("should not effect other functions", function() {
+    expect(bar).toEqual(123);
+  });
+
+  it("when called returns the requested value", function() {
+    expect(fetchedBar).toEqual(123);
+  });
+});
+
+/**
+ ### Spies: `andReturn`
+
+ By chaining the spy with `andReturn`, all calls to the function will return a specific value.
+ */
+describe("A spy, when faking a return value", function() {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      },
+      getBar: function() {
+        return bar;
+      }
+    };
+
+    spyOn(foo, 'getBar').andReturn(745);
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it("should not effect other functions", function() {
+    expect(bar).toEqual(123);
+  });
+
+  it("when called returns the requested value", function() {
+    expect(fetchedBar).toEqual(745);
+  });
+});
+
+/**
+ ### Spies: `andCallFake`
+
+ By chaining the spy with `andCallFake`, all calls to the spy will delegate to the supplied function.
+ */
+describe("A spy, when faking a return value", function() {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      },
+      getBar: function() {
+        return bar;
+      }
+    };
+
+    spyOn(foo, 'getBar').andCallFake(function() {
+      return 1001;
+    });
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it("should not effect other functions", function() {
+    expect(bar).toEqual(123);
+  });
+
+  it("when called returns the requested value", function() {
+    expect(fetchedBar).toEqual(1001);
+  });
+});
+
+/**
+ ### Spies: `createSpy`
+
+ When there is not a function to spy on, `jasmine.createSpy` can create a "bare" spy. This spy acts as any other spy - tracking calls, arguments, etc. But there is no implementation behind it. Spies are JavaScript objects and can be used as such.
+
+ */
+describe("A spy, when created manually", function() {
+  var whatAmI;
+
+  beforeEach(function() {
+    whatAmI = jasmine.createSpy('whatAmI');
+
+    whatAmI("I", "am", "a", "spy");
+  });
+
+  it("is named, which helps in error reporting", function() {
+    expect(whatAmI.identity).toEqual('whatAmI')
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(whatAmI).toHaveBeenCalled();
+  });
+
+  it("tracks its number of calls", function() {
+    expect(whatAmI.calls.length).toEqual(1);
+  });
+
+  it("tracks all the arguments of its calls", function() {
+    expect(whatAmI).toHaveBeenCalledWith("I", "am", "a", "spy");
+  });
+
+  it("allows access to the most recent call", function() {
+    expect(whatAmI.mostRecentCall.args[0]).toEqual("I");
+  });
+});
+
+/**
+ ### Spies: `createSpyObj`
+
+ In order to create a mock with multiple spies, use `jasmine.createSpyObj` and pass an array of strings. It returns an object that has a property for each string that is a spy.
+ */
+describe("Multiple spies, when created manually", function() {
+  var tape;
+
+  beforeEach(function() {
+    tape = jasmine.createSpyObj('tape', ['play', 'pause', 'stop', 'rewind']);
+
+    tape.play();
+    tape.pause();
+    tape.rewind(0);
+  });
+
+  it("creates spies for each requested function", function() {
+    expect(tape.play).toBeDefined();
+    expect(tape.pause).toBeDefined();
+    expect(tape.stop).toBeDefined();
+    expect(tape.rewind).toBeDefined();
+  });
+
+  it("tracks that the spies were called", function() {
+    expect(tape.play).toHaveBeenCalled();
+    expect(tape.pause).toHaveBeenCalled();
+    expect(tape.rewind).toHaveBeenCalled();
+    expect(tape.stop).not.toHaveBeenCalled();
+  });
+
+  it("tracks all the arguments of its calls", function() {
+    expect(tape.rewind).toHaveBeenCalledWith(0);
+  });
+});
+
+/**
+ ## Matching Anything with `jasmine.any`
+
+ `jasmine.any` takes a constructor or "class" name as an expected value. It returns `true` if the constructor matches the constructor of the actual value.
+ */
+
+describe("jasmine.any", function() {
+  it("matches any value", function() {
+    expect({}).toEqual(jasmine.any(Object));
+    expect(12).toEqual(jasmine.any(Number));
+  });
+
+  describe("when used with a spy", function() {
+    it("is useful for comparing arguments", function() {
+      var foo = jasmine.createSpy('foo');
+      foo(12, function() {
+        return true
+      });
+
+      expect(foo).toHaveBeenCalledWith(jasmine.any(Number), jasmine.any(Function));
+    });
+  });
+});
+
+/**
+ ## Mocking the JavaScript Clock
+
+ The Jasmine Mock Clock is available for a test suites that need the ability to use `setTimeout` or `setInterval` callbacks. It makes the timer callbacks synchronous, thus making them easier to test.
+
+ */
+describe("Manually ticking the Jasmine Mock Clock", function() {
+  var timerCallback;
+
+  /**
+   It is installed with a call to `jasmine.Clock.useMock` in a spec or suite that needs to call the timer functions.
+   */
+  beforeEach(function() {
+    timerCallback = jasmine.createSpy('timerCallback');
+    jasmine.Clock.useMock();
+  });
+
+  /**
+   Calls to any registered callback are triggered when the clock is ticked forward via the `jasmine.Clock.tick` function, which takes a number of milliseconds.
+   */
+  it("causes a timeout to be called synchronously", function() {
+    setTimeout(function() {
+      timerCallback();
+    }, 100);
+
+    expect(timerCallback).not.toHaveBeenCalled();
+
+    jasmine.Clock.tick(101);
+
+    expect(timerCallback).toHaveBeenCalled();
+  });
+
+  it("causes an interval to be called synchronously", function() {
+    setInterval(function() {
+      timerCallback();
+    }, 100);
+
+    expect(timerCallback).not.toHaveBeenCalled();
+
+    jasmine.Clock.tick(101);
+    expect(timerCallback.callCount).toEqual(1);
+
+    jasmine.Clock.tick(50);
+    expect(timerCallback.callCount).toEqual(1);
+
+    jasmine.Clock.tick(50);
+    expect(timerCallback.callCount).toEqual(2);
+  });
+});
+
+/**
+ ## Asynchronous Support
+
+ Jasmine also has support for running specs that require testing asynchronous operations.
+
+ */
+describe("Asynchronous specs", function() {
+  var value, flag;
+
+  it("should support async execution of test preparation and exepectations", function() {
+
+    /**
+     Specs are written by defining a set of blocks with calls to `runs`, which usually finish with an asynchronous call.
+     */
+    runs(function() {
+      flag = false;
+      value = 0;
+
+      setTimeout(function() {
+        flag = true;
+      }, 500);
+    });
+
+    /**
+     The `waitsFor` block takes a latch function, a failure message, and a timeout.
+
+     The latch function polls until it returns true or the timeout expires, whichever comes first. If the timeout expires, the spec fails with the error message.
+     */
+    waitsFor(function() {
+      value++;
+      return flag;
+    }, "The Value should be incremented", 750);
+
+    /**
+     Once the asynchronous conditions have been met, another `runs` block defines final test behavior. This is usually expectations based on state after the asynch call returns.
+     */
+    runs(function() {
+      expect(value).toBeGreaterThan(0);
+    });
+  });
+});
 
 /**
  ## The Runner and Reporter
